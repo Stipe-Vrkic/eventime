dd23fc79711ecb18f22d8e81df75046611aeb358
Stipe Vrkić <72603268+Stipe-Vrkic@users.noreply.github.com>
2020-11-17 21:17:05 +0100

    contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,32 +1,19 @@
 <template>
-     <!-- Contact-->
-        <section class="page-section" id="contact">
-            <div class="container">
-                <div class="text-center">
-                    <h2 class="section-heading text-uppercase">Kontakirajte nas!</h2>
-                    <h3 class="section-subheading text-muted">Na vaše pitanje odgovaramo odmah</h3>
-                </div>
-<form name="contact" netlify>
+  <form name="simple-form" method="POST" data-netlify="true">
   <p>
-    <label>Name <input type="text" name="name" /></label>
+    <label>Your Name: <input type="text" name="name"/></label>
   </p>
   <p>
-    <label>Email <input type="email" name="email" /></label>
+    <label>Your Email: <input type="email" name="email"/></label>
   </p>
   <p>
-    <button type="submit">Send</button>
+    <label>Message: <input type="textarea" name="message"/></label>
   </p>
+  
+  <p><button type="submit">Send</button></p>
+   <input type="hidden"  name="form-name" value="simple-form" /><input>
 </form>
-            </div>
-        </section>
 </template>
-
 <script>
-export default {
-    
-}
+
 </script>
-
-<style>
-
-</style>
