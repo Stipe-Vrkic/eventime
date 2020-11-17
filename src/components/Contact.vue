5bd5a961ac9182f8d096c340756ab2b732f75173
Stipe Vrkić <72603268+Stipe-Vrkic@users.noreply.github.com>
2020-11-17 21:50:23 +0100

    update

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,38 +1,51 @@
 <template>
-  <form
-    name="ask-question"
-    method="post"
-    data-netlify="true"
-    data-netlify-honeypot="bot-field"
-    >
-    <input type="hidden" name="form-name" value="ask-question" />
-    <label v-for="(panelist, index) in panelists" :key="index">
-      <input
-        type="radio"
-        name="panelist"
-        :value="panelist"
-        @input="ev => updatePanelist"
-        :checked="panelist === currentPanelist"
-      />
-      <span>{{ panelist }}</span>
-    </label>
-    ...
-    <button>Submit</button>
-  </form>
+     <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Kontakirajte nas!</h2>
+                    <h3 class="section-subheading text-muted">Na vaše pitanje odgovaramo odmah</h3>
+                </div>
+                <form   name="ask-question" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                 <input type="hidden" name="form-name" value="ask-question" />
+                                <input class="form-control" id="name" name="ime" type="text" placeholder="Ime i prezime *" required="required" data-validation-required-message="Please enter your name." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" id="email" name="email" type="email" placeholder="Email *" required="required" data-validation-required-message="Please enter your email address." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <input class="form-control" id="phone" name="telefon" type="tel" placeholder="Mobitel *" required="required" data-validation-required-message="Please enter your phone number." />
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group form-group-textarea mb-md-0">
+                                <textarea class="form-control" id="message" placeholder="Poruka *" name="poruka" required="required" data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="text-center">
+                        <div id="success"></div>
+                        <button class="btn btn-primary btn-xl text-uppercase" id="sendMessageButton" type="submit">Pošalji poruku</button>
+                    </div>
+                    
+                </form>
+            </div>
+        </section>
 </template>
+
 <script>
 export default {
-  name: "QAForm",
-  methods: {
-    updatePanelist (ev) {
-      this.currentPanelist = ev.target.value
-    }
-  },
-  data () {
-    return {
-      panelists: ['Evan You', 'Chris Fritz'],
-      currentPanelist: 'Evan You'
-    }
-  }
+    
 }
 </script>
+
+<style>
+
+</style>
