4c63b454f8c05cb78facc922c7146c06c0f46f14
Stipe Vrkić <72603268+Stipe-Vrkic@users.noreply.github.com>
2020-11-17 21:38:23 +0100

    sda
    dsa

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,22 +1,38 @@
 <template>
-  <form name="simple-form" method="POST" data-netlify="true">
-      
-     <input type="hidden" name="form-name" value="simple-form"/>
-      
-  <p>
-    <label>Your Name: <input type="text" name="name"/></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email"/></label>
-  </p>
-  <p>
-    <label>Message: <input type="textarea" name="message"/></label>
-  </p>
-  
-  <p><button type="submit">Send</button></p>
-  
-</form>
+  <form
+    name="ask-question"
+    method="post"
+    data-netlify="true"
+    data-netlify-honeypot="bot-field"
+    >
+    <input type="hidden" name="form-name" value="ask-question" />
+    <label v-for="(panelist, index) in panelists" :key="index">
+      <input
+        type="radio"
+        name="panelist"
+        :value="panelist"
+        @input="ev => updatePanelist"
+        :checked="panelist === currentPanelist"
+      />
+      <span>{{ panelist }}</span>
+    </label>
+    ...
+    <button>Submit</button>
+  </form>
 </template>
 <script>
-
+export default {
+  name: "QAForm",
+  methods: {
+    updatePanelist (ev) {
+      this.currentPanelist = ev.target.value
+    }
+  },
+  data () {
+    return {
+      panelists: ['Evan You', 'Chris Fritz'],
+      currentPanelist: 'Evan You'
+    }
+  }
+}
 </script>
