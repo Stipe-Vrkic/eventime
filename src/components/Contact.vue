8c382141c74016a4af42459f960caabefd6d7619
Stipe Vrkić <72603268+Stipe-Vrkic@users.noreply.github.com>
2020-11-17 21:06:50 +0100

    Update Contact.vue

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,21 +6,12 @@
                     <h2 class="section-heading text-uppercase">Kontakirajte nas!</h2>
                     <h3 class="section-subheading text-muted">Na vaše pitanje odgovaramo odmah</h3>
                 </div>
-            <form name="contact" action="POST" data-netlify="true">
+<form name="contact" netlify>
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
+    <label>Name <input type="text" name="name" /></label>
   </p>
   <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
+    <label>Email <input type="email" name="email" /></label>
   </p>
   <p>
     <button type="submit">Send</button>
